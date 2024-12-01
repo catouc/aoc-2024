4ad3ff2925df353020650d858336677e47a1ad8b
Philipp Böschen <catouc@philipp.boeschen.me>
2024-12-01 12:31:55 +0100

    Fix declaring empty hash

--- 01/day1-2.pl
+++ 01/day1-2.pl
@@ -3,7 +3,7 @@ use v5.36;
 $|++;
 
 my @left = [];
-my %counts = {};
+my %counts;
 foreach my $line ( <STDIN> ) {
 	($a, $b) = $line =~ /(\d+)\s+(\d+)/;
 	$counts{$b} += 1;
@@ -14,12 +14,7 @@ foreach my $line ( <STDIN> ) {
 
 my $sum = 0;
 for my $i (0 .. $#left-1) {
-	my $diff = 0;
-	if (defined $counts{$left[$i]}) {
-		$diff = abs($left[$i] * $counts{$left[$i]});
-	} else {
-		$diff = 0;
-	}
+	my $diff = abs($left[$i] * ($counts{$left[$i]} // 0));
 	$sum += $diff;
 }
 
